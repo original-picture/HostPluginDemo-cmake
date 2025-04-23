c3f99b23cf1084bf434f290f0685b38d518b7390
Dexter <dexterwest317@gmail.com>
2025-04-22 19:16:31 -0700

    Still haven't figured out what the issue is

--- native_window_system_impls/x11.cpp
+++ native_window_system_impls/x11.cpp
@@ -1,51 +1,41 @@
 
 #include "X11/Xlib.h"
-#include "X11/X.h"
-#include "X11/Xutil.h"
-#include "X11/Xcursor/Xcursor.h"
-#include "X11/extensions/Xinerama.h"
-#include "X11/extensions/Xrandr.h"
-#include "X11/extensions/XShm.h"
-
-#include "juce_gui_basics/native/juce_XSymbols_linux.h"
 
 #include <string>
 
 std::string get_x_error(int code) {
     std::string ret(1024, ' ');
 
-    juce::X11Symbols::getInstance()->xGetErrorText(juce::X11Symbols::getInstance()->xOpenDisplay(nullptr), code, ret.data(), 1024);
+    XGetErrorText(XOpenDisplay(nullptr), code, ret.data(), 1024);
 
     return ret;
 }
 
 int handler(Display * d, XErrorEvent * e)
 {
-
+    return 0; // the handler does nothing, I just set a breakpoint in it to see if it gets called. It never does
 }
 
 void set_handler() {
-    juce::X11Symbols::getInstance()->xSetErrorHandler(handler);
+    XSetErrorHandler(handler);
 }
 
 void set_component_native_owning_window(juce::Component& to_be_owned, juce::Component& to_be_owner) {
 
-    int ret;
-    auto display = juce::X11Symbols::getInstance()->xOpenDisplay(nullptr);
-
-
+    auto display = XOpenDisplay(nullptr);
 
     if(display) {
-        if(ret = juce::X11Symbols::getInstance()->xSetTransientForHint(display,
-                reinterpret_cast<Window>(to_be_owned.getWindowHandle()),
-                reinterpret_cast<Window>(to_be_owner.getWindowHandle()))) { // failure
-            //to_be_owned.setAlwaysOnTop(true); // fallback behavior. Not ideal but close enough to what we want
+        auto to_be_owned_window = reinterpret_cast<Window>(to_be_owned.getWindowHandle()),
+             to_be_owner_window = reinterpret_cast<Window>(to_be_owner.getWindowHandle());
+
+        if(int ret = XSetTransientForHint(display, to_be_owner_window,
+                                                   to_be_owned_window)) { // failure
+            //to_be_owned.setAlwaysOnTop(true); // doesn't do anything on linux
 
             auto str = get_x_error(ret);
-
         }
     }
-    else {
+    else { // XOpenDisplay failed
 
     }
 }
